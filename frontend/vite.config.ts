d8c1557ca3f03c880ebaa0ff7f3c3edb070de39b
alex <alexmal24122004@gmail.com>
2025-10-14 14:39:49 +0300

    start doing frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:8080',
+      '/health': 'http://localhost:8080',
+      '/swagger': 'http://localhost:8080'
+    }
+  }
+})
+
